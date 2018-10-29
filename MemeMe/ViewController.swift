0369e7431a3b0b27f75706dcb4abde9dc3beec24
dhlong <dhlong@dhlongs-imac.local>
2018-10-29 07:56:55 -0700

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by dhlong on 29/10/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
